45a4fa47858bb81e44435b75256cc60707d12fbc
Hieu Ngan <21520358@gm.uit.edu.vn>
2023-10-21 11:22:37 +0700

    test connect db

---
+++ EShop/Data/EShopDBContext.cs
@@ -0,0 +1,24 @@
+﻿using EShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Data
+{
+    public class EShopDBContext: DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server = LAPTOP-PP5O9HD5; Database = Test; Trusted_Connection = True; Integrated Security=True; TrustServerCertificate = True; MultipleActiveResultSets = True");
+            }
+        }
+    }
+
+
+
+    
+
+
+}
---
+++ EShop/Services/TestService/ITestService.cs
@@ -0,0 +1,10 @@
+﻿using EShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Services.TestService
+{
+    public interface ITestService
+    {
+        Task<ActionResult<IEnumerable<Test>>> GetAll(); 
+    }
+}
