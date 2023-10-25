098c9e7c5114a3d763f31df547ee310f7dc882ac
Hieu Ngan <21520358@gm.uit.edu.vn>
2023-10-25 15:08:26 +0700

    coupon model

---
+++ EShop/Models/CouponModel/Coupon.cs
@@ -0,0 +1,14 @@
+﻿namespace EShop.Models.CouponModel
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string Desciption { get; set; }
+        public ApplyCouponType ApplyCouponType { get; set; }
+        public double DiscountPercent { get; set; }
+        public double DiscountAmount { get; set; }  
+        public double MaxDiscountAmount { get; set; }   
+        public double MinBillAmount { get; set; }   
+      
+    }
+}
